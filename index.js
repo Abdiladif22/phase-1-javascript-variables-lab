103d7a756e7a39c013acb64abfb2aa313cf0018e
Abdiladi22 <abdiladifali0@gmail.com>
2023-03-20 23:01:41 +0300

    compelet assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+let companyName =('Scuber');    console.log(companyName);
+//const companyName = 'Scuber';
+let mostProfitableNeighborhood = "Chelsea";console.log(mostProfitableNeighborhood);let companyCeo = "Susan Smith";
+console.log(companyCeo);
+
